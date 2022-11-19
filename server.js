0ecad5f76089c4a913ce47cc74fa76f99a2f71b1
ofir-frd <ofir.friedman@materials.zone>
2022-11-19 19:00:18 +0200

    mutation of a book

--- server.js
+++ server.js
@@ -94,9 +94,29 @@ const RootQueryType = new GraphQLObjectType({
     })
 })
 
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            description: "Add a book",
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId}
+                books.push(book)
+                return book
+            }
+        }
+    })
+})
 
 const schema = new GraphQLSchema({
-  query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMutationType
 })
 
 app.use('/graphql', expressGraphQL({

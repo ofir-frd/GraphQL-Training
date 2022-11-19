76a67db52458246483482ffb497ae9019acdec87
ofir-frd <ofir.friedman@materials.zone>
2022-11-19 18:45:51 +0200

    Find a single book

--- server.js
+++ server.js
@@ -65,6 +65,14 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A single Books',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id)
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of All Books',
